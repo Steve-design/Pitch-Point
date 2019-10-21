da92cbdb388acbc88d24ff4e197c2efc9ba9d40f
Steve-design <smzalendo31@gmail.com>
2019-10-21 19:24:06 +0300

    create a class for comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,2 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Your comment...')
+    submit = SubmitField('Submit')
