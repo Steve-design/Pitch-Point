cd6fbaea2dd26700817ee95bae3438d70ae7f611
Steve-design <smzalendo31@gmail.com>
2019-10-21 18:39:56 +0300

    add an app configuration function

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,3 +4,6 @@ from . import views,error
 
 def create_app(config_name):
     app = Flask(__name__)
+
+     # Creating the app configurations
+    app.config.from_object(config_options[config_name])
