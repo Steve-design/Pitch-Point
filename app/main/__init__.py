916c2d6498dbdeae618df5624986e454411e5929
Steve-design <smzalendo31@gmail.com>
2019-10-21 18:45:02 +0300

    add a function to register the blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -7,6 +7,12 @@ def create_app(config_name):
 
      # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    
+
      # Initializing flask extensions
     bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
