b11b98a81d425f7886bf71b00698eb52e38e6f78
Steve-design <smzalendo31@gmail.com>
2019-10-22 09:29:02 +0300

    create instances for db, simple, bootstrap and mail

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,10 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+
+
+simple = SimpleMDE()
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
