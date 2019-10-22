2ac8b01721313cd6040c880a190389ca533e42b0
Steve-design <smzalendo31@gmail.com>
2019-10-22 10:03:15 +0300

    add a development configuration class

--- config.py
+++ config.py
@@ -30,3 +30,20 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     DEBUG =  True    
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nancy:mxmxmx@localhost/pitch'
+
+    '''
+    
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}  
