bad6a5d4184dd3d22a819669af42033e2d080936
Steve-design <smzalendo31@gmail.com>
2019-10-22 16:55:37 +0300

    install unicorn

--- manage.py
+++ manage.py
@@ -3,10 +3,6 @@ from app.models import User,Pitch,Comment
 from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
 
-app = create_app('production')
-
-# app = create_app('test')
-
 app = create_app('development')
 
 
