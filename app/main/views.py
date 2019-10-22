b09277008a228f36ed2c5202950d59d1aa231174
Steve-design <smzalendo31@gmail.com>
2019-10-22 17:17:44 +0300

    import modules

---
+++ app/main/views.py
@@ -0,0 +1,7 @@
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Comment,User,Pitch
+from . import main
+from .forms import CommentForm, PitchForm,UpdateProfile
+from flask_login import login_required, current_user
+from .. import db,photos
+import markdown2
