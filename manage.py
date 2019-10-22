51ecd8fddfa5d151093f08c24654a42e70577249
Steve-design <smzalendo31@gmail.com>
2019-10-22 09:10:29 +0300

    import Manager, server, Pitch and Comment classes

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from app import create_app,db
-from app.models import User
+from app.models import User,Pitch,Comment
+from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
 
 @manager.shell
 def make_shell_context():
